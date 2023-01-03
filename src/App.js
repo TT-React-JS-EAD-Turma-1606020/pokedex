226a33c585680b1a7ee41ad10b1308234b505e01
Luciel Santos <luciel.vitor.bs@gmail.com>
2023-01-02 22:16:00 -0300

    feito configuração base das rotas da aplicação, com react router dom

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Header } from "./components/Header";
 import { Home } from "./pages/Home";
 import { GlobalStyles } from "./styles/global";
 
+
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
 
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
 
       <GlobalStyles />
-    </>
+    </BrowserRouter>
   );
 }
 
